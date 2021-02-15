ddee37392fd73de812b1544521d586f5ab7ef9f6
lucas-bruzzone <lucas.rbruzzone@gmail.com>
2021-02-15 14:24:55 -0300

    First commit

---
+++ Aula1/eventos.py
@@ -0,0 +1,28 @@
+import boto3
+import json
+from fake_web_events import Simulation
+
+
+client = boto3.client('kinesis', 
+region_name='us-east-2', 
+##aws_access_key_id = '', 
+##aws_secret_access_key = '')
+
+
+def put_record(event):
+    data = (json.dumps(event) + '\n').encode('utf-8')
+    response = client.put_record(
+        StreamName='kinesis-stream',
+        Data=data,
+        PartitionKey='test'
+    )
+    print(event)
+    return response
+
+
+simulation = Simulation(user_pool_size=100, sessions_per_day=10000)
+events = simulation.run(duration_seconds=300)
+
+for event in events:
+    put_record(event)
+    
