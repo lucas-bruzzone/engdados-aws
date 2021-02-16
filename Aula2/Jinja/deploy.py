7c22f876e81cdbb5ef6051316a3cf587767ec03e
lucas-bruzzone <lucas.rbruzzone@gmail.com>
2021-02-16 14:45:03 -0300

    correção path

--- Aula2/Jinja/deploy.py
+++ Aula2/Jinja/deploy.py
@@ -79,18 +79,20 @@ def create_or_update_stack():
 
 def renderiza_template():
     logging.info(f'RENDERING JINJA')
-    with open(_get_abs_path('Aula2/Jinja/redshift.yml.j2'), 'r') as f:
+    
+    with open('Aula2/Jinja/redshift.yml.j2', 'r') as f:
         redshift_yaml = f.read()
 
-    with open(_get_abs_path('Aula2/Jinja/config.yml'), 'r') as f:
+    with open('Aula2/Jinja/config.yml', 'r') as f:
         config = yaml.safe_load(f)
 
 
     redshift_template = jinja2.Template(redshift_yaml)
     redshift_rendered = redshift_template.render({**config, **os.environ})
 
-    with open(_get_abs_path('Aula2/Jinja/redshift.yml'), 'w') as f:
+    with open('Aula2/Jinja/redshift.yml', 'w') as f:
         f.write(redshift_rendered)
+
     logging.info(f'JINJA RENDERED')
 
 
